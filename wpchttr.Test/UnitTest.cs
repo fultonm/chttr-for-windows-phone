6928b3201e20b796a206e1ebd07e55e47b988f78
fultonm <fultonm@wartimestudios.com>
2015-10-08 12:27:00 -0700

    Remade test project. Universal apps still rough around edges MS.

--- wpchttr.Test/UnitTest.cs
+++ wpchttr.Test/UnitTest.cs
@@ -11,14 +11,7 @@ namespace wpchttr.Test
         public void SignIn_ValidCredentials_ReturnsTrue()
         {
             CurrentUser cu = new CurrentUser("fultonm@wartimestudios.com", "mike2009");
-            Assert.IsTrue(cu.SignIn());              
+            Assert.IsTrue(cu.SignIn());
         }
-
-        //[TestMethod]
-        //public void SignIn_InvalidCredentials_ReturnsFalse()
-        //{
-        //    CurrentUser cu = new CurrentUser("fultonm@invalid", "mike2009");
-        //    Assert.IsFalse(cu.SignIn());
-        //}
     }
 }
